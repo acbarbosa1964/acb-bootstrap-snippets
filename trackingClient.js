b3f3575b4dc87347982992db8cbb0dce285a34c3
acbarbosa1964 <acbarbosa@gmail.com>
2015-10-19 13:50:55 -0200

    Client Tracking

--- trackingClient.js
+++ trackingClient.js
@@ -5,7 +5,7 @@
 // use require('./onlineTrackingClient').init() to activate tracking
 
 define(function(require, exports){
-    var trackingServiceUrl = 'http://brackets-online.herokuapp.com/',
+    var trackingServiceUrl = 'http://online-dnbard.rhcloud.com/',
         // http://brackets-online.herokuapp.com/ is an address of default tracking service
         // Change it if you use self-hosting instance of online tracking service
         appToken = '5620f410e59615263f000006',
